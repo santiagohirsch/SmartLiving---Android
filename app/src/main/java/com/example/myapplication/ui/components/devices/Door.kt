ef812f8bd913630f7ee50c4acbaf37f2ce7a3e0f
smedin <smedin@itba.edu.ar>
2023-06-09 17:11:23 -0300

    door updated

--- app/src/main/java/com/example/myapplication/ui/components/devices/Door.kt
+++ app/src/main/java/com/example/myapplication/ui/components/devices/Door.kt
@@ -1,7 +1,13 @@
 package com.example.myapplication.ui.components.devices
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Switch
 import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
@@ -10,8 +16,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
@@ -28,8 +39,11 @@ fun Door(){
     var lockEnable by remember {
         mutableStateOf(true)
     }
-    Column() {
-
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Row() {
             Switch(
                 checked = switchOpen,
@@ -41,10 +55,16 @@ fun Door(){
                     checkedThumbColor = Color.Green,
                     checkedTrackColor = Color.Gray
                 ),
-                enabled = openEnable
+                enabled = openEnable,
+                modifier = Modifier.scale(2.2f)
             )
-            Text(text = if (switchOpen) "Open" else "Close")
+            Spacer(modifier = Modifier.width(23.dp))
+            Text(
+                text = if (switchOpen) "Open" else "Close",
+                fontSize = 30.sp
+            )
         }
+        Spacer(modifier = Modifier.height(20.dp))
         Row() {
             Switch(
                 checked = switchLock,
@@ -56,9 +76,14 @@ fun Door(){
                     checkedThumbColor = Color.Green,
                     checkedTrackColor = Color.Gray
                 ),
-                enabled = lockEnable
+                enabled = lockEnable,
+                modifier = Modifier.scale(2.2f)
             )
-            Text(text = if (switchLock) "Locked" else "Unlocked")
+            Spacer(modifier = Modifier.width(23.dp))
+            Text(
+                text = if (switchLock) "Locked" else "Unlocked",
+                fontSize = 30.sp
+            )
         }
 
     }
