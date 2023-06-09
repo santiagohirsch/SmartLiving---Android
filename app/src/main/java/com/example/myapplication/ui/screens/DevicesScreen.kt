ff520aa3a924b670994aed7d0ee04ff0bf3b6ef6
smedin <smedin@itba.edu.ar>
2023-06-09 17:12:05 -0300

    add button

--- app/src/main/java/com/example/myapplication/ui/screens/DevicesScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/DevicesScreen.kt
@@ -1,17 +1,26 @@
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -25,7 +34,13 @@ import com.example.myapplication.R
 @Composable
 fun DevicesScreen() {
     Surface(modifier = Modifier.fillMaxSize(), color = Color(android.graphics.Color.parseColor("#2f2f4d"))) {
-        Column() {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            //verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
                 text = stringResource(R.string.devicesTitle),
                 fontSize = 25.sp,
@@ -33,11 +48,11 @@ fun DevicesScreen() {
                 textAlign = TextAlign.Center,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 16.dp, top = 8.dp)
+                    .padding(top = 16.dp)
+                    .padding(start = 25.dp)
             )
 
             LazyRow(
-                modifier = Modifier.fillMaxSize(),
                 contentPadding = PaddingValues(16.dp)
             ) {
                 items(10) { index ->
@@ -49,6 +64,20 @@ fun DevicesScreen() {
                     }
                 }
             }
+            Button(
+                onClick = {
+                    /*TODO*/
+                }
+            ) {
+               Text(text = "Agregar dispositivo")
+            }
+            Box(
+                modifier = Modifier
+                .width(150.dp)
+                .height(200.dp)
+                .clip(shape = RoundedCornerShape(size = 22.dp))
+                .background(color = Color.Black)
+            )
         }
     }
 }
