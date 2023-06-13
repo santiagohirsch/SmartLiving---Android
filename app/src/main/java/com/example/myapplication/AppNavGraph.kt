3445100948201693e6a31e436783359151fec459
brunibaumgart <bbaumgart@itba.edu.ar>
2023-06-13 16:53:07 -0300

    NavGraph correction

--- app/src/main/java/com/example/myapplication/AppNavGraph.kt
+++ app/src/main/java/com/example/myapplication/AppNavGraph.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.myapplication.ui.components.DeviceCard
 import com.example.myapplication.ui.screens.LandingScreen
 import com.example.myapplication.ui.screens.RoutinesScreen
 
@@ -19,7 +18,7 @@ fun AppNavGraph(navController: NavHostController) {
             LandingScreen(navController)
         }
         composable(Screen.SecondScreen.route) {
-            DeviceCard(deviceName = "Medin", type = "lamp" , imageRes = R.drawable.lampara)
+            DevicesScreen()
         }
         composable(Screen.ThirdScreen.route) {
             RoutinesScreen()
